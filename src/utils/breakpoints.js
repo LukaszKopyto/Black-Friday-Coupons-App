5a4ae56983ee4faca0d1d9fb0ff8441396710103
Lukasz <lukasz.kopyto@gmail.com>
2020-04-25 12:15:11 +0200

    adding breakpoints to styled-components theme

---
+++ src/utils/breakpoints.js
@@ -0,0 +1,13 @@
+const breakpoints = {
+  sm: '576px',
+  md: '768px',
+  lg: '992px',
+  xl: '1200px',
+}
+
+export const device = {
+  sm: `(min-width: ${breakpoints.sm})`,
+  md: `(min-width: ${breakpoints.md})`,
+  lg: `(min-width: ${breakpoints.lg})`,
+  xl: `(min-width: ${breakpoints.xl})`,
+}
--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,7 +1,9 @@
 import { colors } from './colors'
 import { promotedStores } from './promotedStores'
+import { device } from './breakpoints'
 
 export const theme = {
   colors,
   promotedStores,
+  device,
 }
