3f2c2aa062a6dd2bccab554103954346fcb0ac48
Lukasz <lukasz.kopyto@gmail.com>
2020-04-26 16:15:59 +0200

    hiding sideMenu on mobile and tablet

--- src/components/layout/Article/ArticleWrapper.js
+++ src/components/layout/Article/ArticleWrapper.js
@@ -3,4 +3,8 @@ import styled from 'styled-components'
 export const ArticleWrapper = styled.article`
   padding: 30px;
   flex: 1;
+
+  @media ${({ theme }) => theme.device.xl} {
+    padding: 30px 50px;
+  }
 `
--- src/components/layout/SectionInfo/SideMenuWrapper.js
+++ src/components/layout/SectionInfo/SideMenuWrapper.js
@@ -1,19 +1,22 @@
 import styled from 'styled-components'
 
 export const SideMenuWrapper = styled.nav`
-  display: none;
+  display: flex;
+  width: 340px;
+  background: ${({ theme }) => theme.colors.sideMenuBg};
+  position: absolute;
+  transform: translateX(-100%);
+  & ul {
+    list-style: none;
+    margin-top: 40px;
+    padding: 0 15px 0 50px;
+  }
+  & li {
+    margin-bottom: 20px;
+  }
 
   @media ${({ theme }) => theme.device.xl} {
-    display: flex;
-    width: 340px;
-    background: ${({ theme }) => theme.colors.sideMenuBg};
-    & ul {
-      list-style: none;
-      margin-top: 40px;
-      padding: 0 15px 0 50px;
-    }
-    & li {
-      margin-bottom: 20px;
-    }
+    position: relative;
+    transform: translateX(0%);
   }
 `
