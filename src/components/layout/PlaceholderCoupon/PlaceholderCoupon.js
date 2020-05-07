e544b027c6d7333ce17fcb0f4d625adc1e0ed73d
Lukasz <lukasz.kopyto@gmail.com>
2020-05-07 19:01:01 +0200

    displaying different number of coupons depending on the window width

--- src/components/layout/PlaceholderCoupon/PlaceholderCoupon.js
+++ src/components/layout/PlaceholderCoupon/PlaceholderCoupon.js
@@ -2,38 +2,65 @@ import React from 'react'
 import { PlaceholderElement } from './PlaceholderElement'
 import { PlaceholderWrapper } from './PlaceholderWrapper'
 
-const Coupon = () => {
-  const style = {
-    display: 'flex',
+const PlaceholderCoupon = ({ n }) => {
+  const iterationNum = []
+
+  for (let i = 0; i < n; i++) {
+    iterationNum.push(i)
   }
+
   return (
-    <PlaceholderWrapper>
-      <div style={style}>
-        <PlaceholderElement
-          width={'111px'}
-          height={'111px'}
-          margin={'0 16px 0 0'}
-        />
-        <div>
-          <PlaceholderElement width={'87px'} height={'20px'} margin={'6px'} />
+    <>
+      {iterationNum.map((item, id) => (
+        <PlaceholderWrapper key={id}>
+          <div style={{ display: 'flex' }}>
+            <PlaceholderElement
+              width={'111px'}
+              height={'111px'}
+              margin={'0 16px 0 0'}
+            />
+            <div>
+              <PlaceholderElement
+                width={'87px'}
+                height={'20px'}
+                margin={'6px'}
+              />
+              <PlaceholderElement
+                width={'87px'}
+                height={'20px'}
+                margin={'10px 6px'}
+              />
+              <PlaceholderElement
+                width={'87px'}
+                height={'40px'}
+                margin={'15px 6px'}
+              />
+            </div>
+          </div>
           <PlaceholderElement
-            width={'87px'}
+            width={'267px'}
             height={'20px'}
-            margin={'10px 6px'}
+            margin={'0 0 6px'}
           />
           <PlaceholderElement
-            width={'87px'}
-            height={'40px'}
-            margin={'15px 6px'}
+            width={'267px'}
+            height={'20px'}
+            margin={'6px 0'}
           />
-        </div>
-      </div>
-      <PlaceholderElement width={'267px'} height={'20px'} margin={'0 0 6px'} />
-      <PlaceholderElement width={'267px'} height={'20px'} margin={'6px 0'} />
-      <PlaceholderElement width={'267px'} height={'20px'} margin={'6px 0'} />
-      <PlaceholderElement width={'267px'} height={'40px'} margin={'20px 0 0'} />
-    </PlaceholderWrapper>
+          <PlaceholderElement
+            width={'267px'}
+            height={'20px'}
+            margin={'6px 0'}
+          />
+          <PlaceholderElement
+            width={'267px'}
+            height={'40px'}
+            margin={'20px 0 0'}
+          />
+        </PlaceholderWrapper>
+      ))}
+    </>
   )
 }
 
-export default Coupon
+export default PlaceholderCoupon
